2b6dfa76f97934c2ae74e7f5567e3f6f4c9b6469
Madzia <magda.gorska.lena@gmail.com>
2021-09-18 08:11:51 +0200

    improving class Tour

--- src/main/java/pl/sda/entity/Tour.java
+++ src/main/java/pl/sda/entity/Tour.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.math.BigDecimal;
 import java.time.LocalDate;
 @Entity
 public class Tour {
@@ -17,13 +18,24 @@ public class Tour {
     private LocalDate dateOfArrival;
     private Integer tourLength;
     private BoardType boardType;
+    private BigDecimal priceForAdult;
+    private BigDecimal priceForChild;
+    private Integer howManyAdults;
+    private Integer howManyChildren;
 
     public Tour(){}
-    public Tour(LocalDate dateOfDeparture, LocalDate dateOfArrival, Integer tourLength, BoardType boardType) {
+
+    public Tour(LocalDate dateOfDeparture, LocalDate dateOfArrival, Integer tourLength
+            , BoardType boardType, BigDecimal priceForAdult, BigDecimal priceForChild
+            , Integer howManyAdults, Integer howManyChildren) {
         this.dateOfDeparture = dateOfDeparture;
         this.dateOfArrival = dateOfArrival;
         this.tourLength = tourLength;
         this.boardType = boardType;
+        this.priceForAdult = priceForAdult;
+        this.priceForChild = priceForChild;
+        this.howManyAdults = howManyAdults;
+        this.howManyChildren = howManyChildren;
     }
 
     public LocalDate getDateOfDeparture() {
@@ -42,6 +54,22 @@ public class Tour {
         return boardType;
     }
 
+    public BigDecimal getPriceForAdult() {
+        return priceForAdult;
+    }
+
+    public BigDecimal getPriceForChild() {
+        return priceForChild;
+    }
+
+    public Integer getHowManyAdults() {
+        return howManyAdults;
+    }
+
+    public Integer getHowManyChildren() {
+        return howManyChildren;
+    }
+
     public void setDateOfDeparture(LocalDate dateOfDeparture) {
         this.dateOfDeparture = dateOfDeparture;
     }
@@ -56,5 +84,21 @@ public class Tour {
 
     public void setBoardType(BoardType boardType) {
         this.boardType = boardType;
+    }
+
+    public void setPriceForAdult(BigDecimal priceForAdult) {
+        this.priceForAdult = priceForAdult;
+    }
+
+    public void setPriceForChild(BigDecimal priceForChild) {
+        this.priceForChild = priceForChild;
+    }
+
+    public void setHowManyAdults(Integer howManyAdults) {
+        this.howManyAdults = howManyAdults;
+    }
+
+    public void setHowManyChildren(Integer howManyChildren) {
+        this.howManyChildren = howManyChildren;
     }
 }
