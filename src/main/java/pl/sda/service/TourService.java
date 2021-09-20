5221e846a6fd40030978b0cd46cc0081443f9e33
Madzia <magda.gorska.lena@gmail.com>
2021-09-20 11:19:20 +0200

    updating of TourService and TourServiceImpl

--- src/main/java/pl/sda/service/TourService.java
+++ src/main/java/pl/sda/service/TourService.java
@@ -2,10 +2,17 @@ package pl.sda.service;
 
 import pl.sda.entity.Tour;
 
+import java.util.List;
+
 public interface TourService {
     void save(Tour tour);
+
+    Tour getById (Integer id);
+
+    List<Tour> getAll();
 
     void update(Tour tour);
 
     void delete(Integer id);
+
 }
--- src/main/java/pl/sda/service/impl/TourServiceImpl.java
+++ src/main/java/pl/sda/service/impl/TourServiceImpl.java
@@ -5,6 +5,8 @@ import pl.sda.entity.Tour;
 import pl.sda.repository.TourRepository;
 import pl.sda.service.TourService;
 
+import java.util.List;
+
 @Service
 public class TourServiceImpl implements TourService {
 
@@ -17,6 +19,16 @@ public class TourServiceImpl implements TourService {
     @Override
     public void save(Tour tour) {
         tourRepository.save(tour);
+    }
+
+    @Override
+    public Tour getById(Integer id) {
+        return tourRepository.getById(id);
+    }
+
+    @Override
+    public List<Tour> getAll() {
+        return tourRepository.findAll();
     }
 
     @Override
